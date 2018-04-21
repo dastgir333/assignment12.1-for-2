d969df22a7c74d315ee49094b071866898ec9147
dastgir333 <dastgir333@gmail.com>
2018-04-21 18:15:50 +0530

    Add files via upload

---
+++ assignment12.1/src/app/routing/routing.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserDataComponent } from 'app/user-data/user-data.component';
+
+
+const routes: Routes = [
+];
+
+
+export const routedComponents = RouterModule.forRoot(routes)
